f440c30dd2d5d5bfb4dfa33be8f5b21166cf4c93
Vladimir Kamuz <v.kamuz@gmail.com>
2022-08-23 00:28:24 +0300

    Controllers & Data Structure

--- js/app.js
+++ js/app.js
@@ -1,1 +1,50 @@
-console.log("Hello, JavaScript");
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function(){
+	console.log('Item Controller');
+
+	// Data Structure / State
+	const data = {
+		items: [
+			{id: 0, name: 'Steak Dinner', calories: 1200},
+			{id: 1, name: 'Cookie', calories: 400},
+			{id: 2, name: 'Eggs', calories: 300},
+		],
+		currentItem: null,
+		totalCalories: 0
+	}
+
+	// Public methods
+	return {
+		logData: function(){
+			return data;
+		}
+	}
+})();
+
+// UI Controller
+const UICtrl = (function(){
+	console.log('UI Controller');
+
+	// Public methods
+	return {
+
+	}
+})();
+
+// App Controller
+const AppCtrl = (function(ItemCtrl, UICtrl){
+	console.log('App Controller');
+	console.log(ItemCtrl.logData());
+
+	// Public methods
+	return {
+		init: function(){
+			console.log('Initializing App...')
+		}
+	}
+})(ItemCtrl, UICtrl);
+
+// Initializing App
+AppCtrl.init();
